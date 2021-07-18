ec27e5c7e9c8aa55b0a45ff65fd97e1d7f76e9e8
William Sercombe <wsercombe@gmail.com>
2021-07-17 19:02:21 -0700

    Include order parser functionality

--- js/scripts.js
+++ js/scripts.js
@@ -6,13 +6,32 @@ function Order(){
     "large": 15;
     "topping": 2;
     "side": 5;
+    "none": 0;
     }
 }
 
 
 function orderParser (orderType, orderId)
 {
-
+  let value = "none";
+  if(orderType === "pizza")
+  {
+    if(orderId === 1) {
+      value = "small";
+    } else if(orderId === 2) {
+      value = "medium";
+    } else if (orderId === 3) {
+      value = "large";
+    }
+  } else if(orderType === "side") {
+    if (orderId !== 0) {
+      return "side";
+    } 
+  } else if (orderType === "topping"){
+    if (orderId !== 0) {
+      return "topping";
+    }
+  }
 }
 
 
