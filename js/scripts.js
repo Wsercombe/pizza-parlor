21fe5d28b9223a7924db3b961de513bed81e0320
William Sercombe <wsercombe@gmail.com>
2021-07-17 18:56:58 -0700

    Set up initial order object

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,25 @@
+function Order(){
+  self.price = 0;
+  self.priceDict = {
+    "small": 10;
+    "medium": 12;
+    "large": 15;
+    "topping": 2;
+    "side": 5;
+    }
+}
+
+
+function orderParser (orderType, orderId)
+{
+
+}
+
+
+let order = new Order;
+
 $(document).ready(function(event) {
-  $("#aFunction").submit(function(event) {
+  $("#order").submit(function(event) {
+
   });
 });
